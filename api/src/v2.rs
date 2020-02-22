c454d121e32946be2f8775a022c7e3280b938e5d
Brian Yarr <brian.yarr@gmail.com>
2020-02-22 09:04:08 +0000

    Get project

--- api/src/v2.rs
+++ api/src/v2.rs
@@ -6,25 +6,52 @@ const BASE_PATH : &str = "https://circleci.com/api/v2";
 const API_KEY_HEADER: &str = "Circle-Token";
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct Project<'a> {
-    slug: &'a str,
-    name: &'a str,
-    organization_name: &'a str,
+pub struct Project {
+    slug: String,
+    name: String,
+    organization_name: String,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct Pipeline<'a> {
-    id: &'a str,
-    project_slug: &'a str,
-    updated_at: Option<&'a str>,
+pub struct Pipeline {
+    id: String,
+    project_slug: String,
+    updated_at: Option<String>,
     number: i64,
-    state: &'a str,
+    state: String,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct PipelineList<'a> {
-    items: Vec<Pipeline<'a>>,
-    next_page_token: &'a str
+pub struct PipelineList<> {
+    items: Vec<Pipeline>,
+    next_page_token: String
+}
+
+pub struct Client {
+    api_key: String,
+    client: reqwest::blocking::Client,
+}
+
+impl Client {
+
+    pub fn new(api_key: String) -> Result<Self, Box<dyn std::error::Error>> {
+        let client = reqwest::blocking::Client::builder().build()?;
+        Ok(Client {
+            api_key, client
+        })
+    }
+
+    pub fn get_project(&self, slug: &str) -> Result<Project, reqwest::Error> {
+        //GET /project/{project-slug}
+        let url = format!("{}/project/{}", BASE_PATH, slug);
+
+        let resp = self.client.get(&url)
+            .header(API_KEY_HEADER, &self.api_key)
+            .send()?;
+        let resp = resp.error_for_status()?;
+        resp.json()
+
+    }
 }
 
 
--- circ/src/main.rs
+++ circ/src/main.rs
@@ -1,3 +1,13 @@
+use std::env::args;
+
 fn main() {
     println!("Hello, world!");
+    let a: Vec<String> = args().collect();
+    println!("{:?}", a);
+
+
+    let client = api::v2::Client::new(a.get(1).unwrap().to_string()).unwrap();
+    let project = client.get_project("gh/byarr/dug").unwrap();
+
+    println!("{:?}", project);
 }
